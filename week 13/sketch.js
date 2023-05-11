006ed3da193d441a4bed3a7d4ce799f837b6e74a
Sarah m Hauser <123280964+hausermsarah@users.noreply.github.com>
2023-05-11 16:21:59 -0600

    Add files via upload

--- week 13/sketch.js
+++ week 13/sketch.js
@@ -1,33 +1,75 @@
-var playerX, playerY;
+var blockX = 200; 
+var blockY = 200;
 var enemyX, enemyY;
+var enemy2X, Enemy2Y;
 var score;
+var exitX = 0; 
+var exitY = 0; 
+var exitSize = 50; 
+var exitWidth = 40;
+var exitHeight = 40;
+let blocks = [];
 
 function setup() {
   createCanvas(400, 400);
-  playerX = width / 2;
-  playerY = height - 50;
   enemyX = random(width);
   enemyY = 0;
   score = 0;
 }
 
 function draw() {
-  background(0);
-  movePlayer();
+  background(250);
+  
+    stroke(0);
+  strokeWeight(2);
+  
+    fill(123, 123, 200);
+  rect(exitX, exitY, exitWidth, exitHeight);
+  
+    textSize(30); // set the text size
+  fill(100, 200, 123); // set the fill color
+  text("get to the purple box", 300, 300);
+  
+   textSize(20); // set the text size
+  fill(100, 200, 123); // set the fill color
+  text("avoid the balls!", 300, 350);
+  
+    // Check for collision with the exit
+  if (blockX > exitX && blockX < exitX + exitSize &&
+      blockY > exitY && blockY < exitY + exitSize) {
+    textSize(32);
+    text("You Win!", width/2 - 50, height/2);
+  }
+
+  // Draw the border
+  noFill();
+  rect(0, 0, 400, 400);
   moveEnemy();
   checkCollision();
-  drawPlayer();
   drawEnemy();
   drawScore();
-}
 
-function movePlayer() {
-  if (keyIsDown(LEFT_ARROW)) {
-    playerX -= 5;
-  } else if (keyIsDown(RIGHT_ARROW)) {
-    playerX += 5;
+
+fill(123, 255, 123);
+rect(blockX, blockY, 30, 30);
+}
+  
+function keyPressed() {
+  if (key === 'w' || key === 'W') {
+    // move the block up
+    blockY -= 10;
+  } else if (key === 's' || key === 'S') {
+    // move the block down
+    blockY += 10;
+  } else if (key === 'a' || key === 'A') {
+    // move the block left
+    blockX -= 10;
+  } else if (key === 'd' || key === 'D') {
+    // move the block right
+    blockX += 10;
   }
 }
+
 
 function moveEnemy() {
   enemyY += 3;
@@ -38,7 +80,7 @@ function moveEnemy() {
 }
 
 function checkCollision() {
-  let d = dist(playerX, playerY, enemyX, enemyY);
+  let d = dist(blockX, blockY, enemyX, enemyY);
   if (d < 30) {
     score -= 1;
     enemyX = random(width);
@@ -46,10 +88,6 @@ function checkCollision() {
   }
 }
 
-function drawPlayer() {
-  fill(123, 123, 0);
-  rect(playerX, playerY, 50, 50);
-}
 
 function drawEnemy() {
   fill(255, 123, 123);
@@ -60,4 +98,19 @@ function drawScore() {
   textSize(20);
   textAlign(RIGHT);
   text(score, width - 20, 30);
+
+    for (let block of blocks) {
+    fill(100,100,200);
+    rect(block.x, block.y, block.size, block.size);
+  }
+}
+
+function mouseClicked() {
+  // Store the position of the mouse click and create a new block
+  let block = {
+    x: mouseX,
+    y: mouseY,
+    size: 50
+  };
+  blocks.push(block);
 }
